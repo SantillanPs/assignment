1e1bc409d0de5e63dfd57817300d223afae01cb9
SantillanPs <77979928+SantillanPs@users.noreply.github.com>
2025-02-21 01:42:37 +0800

    new assignment finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'profile.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,248 +13,165 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.white,
-        body: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Container(
-              height: 1000,
-              width: 500,
-              decoration: BoxDecoration(
-                  color: Colors.grey[100],
-                  borderRadius: BorderRadius.circular(25)),
-              child: Center(
-                child: Padding(
-                    padding:
-                        const EdgeInsets.only(top: 40, left: 20, right: 20),
-                    child: SignInForm()),
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          leading: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.menu,
+                size: 40,
               )),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(top: 5),
+              child: SizedBox(
+                width: 100, // Adjust width to control overlap
+                height: 100,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 5, // Adjust to shift the back button slightly
+                      child: Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(50),
+                            border: Border.all(
+                                color: Color.fromARGB(255, 148, 148, 148))),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.notifications,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      right: 10, // Moves the second button over the first
+                      child: Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(50),
+                            border: Border.all(
+                                color: Color.fromARGB(255, 148, 148, 148))),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.person,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+          backgroundColor: Colors.transparent,
         ),
-      ),
-    );
-  }
-}
-
-class SignInForm extends StatefulWidget {
-  const SignInForm({super.key});
-
-  @override
-  State<SignInForm> createState() => SignInFormState();
-}
-
-class LoadingButton extends StatefulWidget {
-  const LoadingButton({super.key});
-
-  @override
-  State<LoadingButton> createState() => _LoadingButtonState();
-}
-
-class _LoadingButtonState extends State<LoadingButton> {
-  bool isLoading = false;
-
-  void handleClick() {
-    setState(() {
-      isLoading = true;
-    });
-
-    Future.delayed(Duration(seconds: 5), () {
-      setState(() {
-        isLoading = false;
-      });
-      Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => Profile()));
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(bottom: 30),
-      child: GestureDetector(
-        onTap: isLoading ? null : handleClick,
-        child: Container(
-          height: 50,
-          width: 300,
+        body: Container(
           decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 130, 83, 241),
-              borderRadius: BorderRadius.circular(10)),
-          child: Center(
-              child: isLoading
-                  ? CircularProgressIndicator(
-                      valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                    )
-                  : Text(
-                      "Continue",
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold),
-                    )),
-        ),
-      ),
-    );
-  }
-}
-
-class SignInFormState extends State<SignInForm> {
-  bool _isObscured = true;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(bottom: 15),
-          child: Text(
-            "Create an account",
-            style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
-          ),
-        ),
-        Padding(
-            padding: const EdgeInsets.only(bottom: 45),
-            child: RichText(
-                text: TextSpan(style: TextStyle(), children: [
-              TextSpan(
-                  text: "Already have an account? ",
-                  style: TextStyle(color: Colors.black)),
-              TextSpan(
-                  text: "Login",
-                  style: TextStyle(
-                      color: const Color.fromARGB(255, 130, 83, 241),
-                      fontWeight: FontWeight.bold))
-            ]))),
-        Padding(
-          padding: const EdgeInsets.only(bottom: 25),
-          child: TextField(
-            decoration: InputDecoration(
-                labelText: "Email address",
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide.none)),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(bottom: 10),
-          child: TextField(
-            obscureText: _isObscured,
-            decoration: InputDecoration(
-                labelText: "Password",
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide.none),
-                suffixIcon: IconButton(
-                  icon: Icon(
-                      _isObscured ? Icons.visibility : Icons.visibility_off),
-                  onPressed: () {
-                    setState(() {
-                      _isObscured = !_isObscured;
-                    });
-                  },
-                )),
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.only(bottom: 10),
-          child: SizedBox(
-            width: 300,
-            height: 30,
-            child: Text(
-              "Recovery Password",
-              textAlign: TextAlign.right,
-              style: TextStyle(fontSize: 12, color: Colors.grey[500]),
+              gradient: LinearGradient(colors: [
+            const Color.fromARGB(255, 227, 243, 227),
+            const Color.fromARGB(255, 240, 231, 222)
+          ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20, top: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 100),
+                Text("Total Balance",
+                    style:
+                        TextStyle(fontSize: 40, fontWeight: FontWeight.w600)),
+                Row(
+                  children: [
+                    Text("Debit Card *4863",
+                        style: TextStyle(color: Colors.black, fontSize: 20)),
+                    SizedBox(width: 5),
+                    Image.asset("icons/debit.png", width: 20),
+                  ],
+                ),
+                SizedBox(height: 30),
+                Center(
+                  child: Text("36,280.00 USD",
+                      style:
+                          TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    _actionButton(Icons.arrow_upward, "Send"),
+                    _actionButton(Icons.arrow_downward, "Receive"),
+                    _actionButton(Icons.swap_horiz, "Convert"),
+                    _actionButton(Icons.more_horiz, "Another"),
+                  ],
+                ),
+                SizedBox(height: 30),
+                Text("Recent Transactions",
+                    style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: _transactionTile(
+                      "From: Julia K.", "Money transfer", "\$200.00", true),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: _transactionTile(
+                      "Sketch", "Monthly subscription fee", "-\$12.00", false),
+                ),
+                _transactionTile(
+                    "YouTube", "Monthly subscription fee", "-\$16.00", false),
+              ],
             ),
           ),
         ),
-        LoadingButton(),
-        Padding(
-            padding: EdgeInsets.only(bottom: 30),
-            child: Row(
-              children: [
-                Expanded(
-                    child: Divider(
-                  color: Colors.grey,
-                  thickness: 1,
-                )),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10),
-                  child: Text(
-                    "or sign up with",
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ),
-                Expanded(
-                    child: Divider(
-                  color: Colors.grey,
-                  thickness: 1,
-                ))
-              ],
-            )),
-        Padding(
-          padding: EdgeInsets.only(bottom: 35),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                height: 50,
-                width: 90,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(
-                        color: const Color.fromARGB(255, 221, 221, 221),
-                        width: 1)),
-                child: Icon(Icons.person),
-              ),
-              Container(
-                height: 50,
-                width: 90,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(
-                        color: const Color.fromARGB(255, 221, 221, 221),
-                        width: 1)),
-                child: Icon(Icons.apple),
-              ),
-              Container(
-                height: 50,
-                width: 90,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(
-                        color: const Color.fromARGB(255, 221, 221, 221),
-                        width: 1)),
-                child: Icon(Icons.facebook),
-              ),
-            ],
+      ),
+    );
+  }
+
+  Widget _actionButton(IconData icon, String label) {
+    return Column(
+      children: [
+        CircleAvatar(
+          backgroundColor: Colors.grey[200],
+          radius: 30,
+          child: Icon(icon, size: 30, color: Colors.black),
+        ),
+        SizedBox(height: 5),
+        Text(label, style: TextStyle(fontSize: 14))
+      ],
+    );
+  }
+
+  Widget _transactionTile(
+      String title, String subtitle, String amount, bool isPositive) {
+    return Container(
+      decoration: BoxDecoration(
+          color: const Color.fromARGB(85, 255, 255, 255),
+          borderRadius: BorderRadius.circular(10)),
+      child: ListTile(
+        leading: CircleAvatar(
+            backgroundColor: Colors.grey[200], child: Icon(Icons.history)),
+        title: Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
+        subtitle: Text(subtitle,
+            style: TextStyle(color: const Color.fromARGB(255, 70, 70, 70))),
+        trailing: Text(
+          amount,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: isPositive ? Colors.green : Colors.black,
           ),
         ),
-        Padding(
-            padding: EdgeInsets.all(0),
-            child: RichText(
-                textAlign: TextAlign.center,
-                text: TextSpan(children: [
-                  TextSpan(
-                    text: "By clicking Create account you agree to Recognize",
-                    style: TextStyle(color: Colors.black),
-                  ),
-                  TextSpan(
-                      text: "Terms of use",
-                      style: TextStyle(
-                          color: const Color.fromARGB(255, 130, 83, 241),
-                          fontWeight: FontWeight.bold)),
-                  TextSpan(
-                      text: " and ", style: TextStyle(color: Colors.black)),
-                  TextSpan(
-                      text: "Privacy policy",
-                      style: TextStyle(
-                          color: const Color.fromARGB(255, 130, 83, 241),
-                          fontWeight: FontWeight.bold))
-                ])))
-      ],
+      ),
     );
   }
 }
